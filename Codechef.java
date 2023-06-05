c5b81e3e5ea7ce9eccb48cd209903fb519a3a4ca
parklatha <135324822+parklatha@users.noreply.github.com>
2023-06-05 12:14:17 +0530

    project added

---
+++ Codechef.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+class Codechef{
+public static void main(String args[]) throws java.lang.Exception
+{
+Scanner in=new Scanner(System.in);
+System.out.println("x:");
+int x=in.nextInt();
+System.out.println("y:");
+int y=in.nextInt();
+System.out.println("h:");
+int h=in.nextInt();
+int z=1*x +1*y +(h-1)*y;
+int c=x+(h-1)*y;
+System.out.println(c);
+}
+
+}
